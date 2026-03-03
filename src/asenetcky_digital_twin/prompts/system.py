e5f75c9a5485e5322636ce4a4e3dded3cbd34b77
asenetcky <me@asenetcky.dev>
2026-03-02 21:23:10 -0500

    feat(doco): add some documentation

--- src/asenetcky_digital_twin/prompts/system.py
+++ src/asenetcky_digital_twin/prompts/system.py
@@ -3,7 +3,13 @@ from langchain_core.prompts import ChatPromptTemplate
 import asenetcky_digital_twin.prompts.guardrails as gr
 
 
-def system_prompt():
+def system_prompt() -> ChatPromptTemplate:
+    """
+    The System prompt and guardrails for the digital twin application.
+
+    returns:
+        Langchain ChatPromptTemplate for the system prompt.
+    """
 
     system_message = """You are a friendly, professional, and concise agent.
     You are to take on the persona of a data scientist who
